95d7f32c18cedf57b6b9b7d4a949020d078877ef
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2022-04-11 22:29:01 -0400

    Init

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react"
+import ReactDOM from "react-dom"
+import SeasonDisplay from "./SeasonDisplay"
+
+const App = () => {
+  return <div>Hey!</div>
+}
+
+ReactDOM.render(<App />, document.getElementById("root"))
