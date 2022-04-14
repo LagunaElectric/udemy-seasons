dafa868431a6899b72ea04beb1810d7e83f36b28
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2022-04-14 00:03:10 -0400

    Added seasonal icons :D

--- src/SeasonDisplay.jsx
+++ src/SeasonDisplay.jsx
@@ -11,9 +11,12 @@ const getSeason = (lat, month) => {
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth())
   const message = season === "winter" ? "Burr, it's chilly!" : "Let's hit the beach!"
+  const icon = season === "winter" ? "snowflake" : "sun"
   return (
     <div>
+      <i className={`icon ${icon}`} />
       <h1>{message}</h1>
+      <i className={`icon ${icon}`} />
     </div>
   )
 }
