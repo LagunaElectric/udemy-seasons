55f269c119ce23000e4a7678476434dbc5b5e289
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2022-04-14 00:08:45 -0400

    Refactored SeasonDisplay to clean up duplicate ternaries

--- src/SeasonDisplay.jsx
+++ src/SeasonDisplay.jsx
@@ -1,5 +1,16 @@
 import React from "react"
 
+const seasonConfig = {
+  summer: {
+    text: "Let's hit the beach!",
+    iconName: "sun"
+  },
+  winter: {
+    text: "Burr, it's chilly!",
+    iconName: "snowflake"
+  }
+}
+
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
     return lat > 0 ? "summer" : "winter"
@@ -10,13 +21,13 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = (props) => {
   const season = getSeason(props.lat, new Date().getMonth())
-  const message = season === "winter" ? "Burr, it's chilly!" : "Let's hit the beach!"
-  const icon = season === "winter" ? "snowflake" : "sun"
+  const { text, iconName } = seasonConfig[season]
+
   return (
     <div>
-      <i className={`icon ${icon}`} />
-      <h1>{message}</h1>
-      <i className={`icon ${icon}`} />
+      <i className={`icon ${iconName}`} />
+      <h1>{text}</h1>
+      <i className={`icon ${iconName}`} />
     </div>
   )
 }
